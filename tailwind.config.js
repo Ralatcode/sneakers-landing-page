b998f9a338db14a1f4901a66a6b6ae0863336449
RalatPC <ralatrado@gmail.com>
2022-11-30 09:12:57 +0100

    added screen size dimension and custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./**/*.html"],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '769px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        Orange: 'hsl(26, 100%, 55%)',
+        PaleOrange: 'hsl(25, 100%, 94%)',
+        VeryDarkBlue: 'hsl(220, 13%, 13%)',
+        DarkGrayishBlue: 'hsl(219, 9%, 45%)',
+        GrayishBlue: 'hsl(220, 14%, 75%)',
+        LightGrayishBlue: 'hsl(223, 64%, 98%)',
+      },
+    },
   },
   plugins: [],
 }
