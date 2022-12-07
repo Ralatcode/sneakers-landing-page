cbabc7f0f3fa9b7b82261382835c3120552cbd87
RalatPC <ralatrado@gmail.com>
2022-12-07 01:48:33 +0100

    fixed increment issues

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
+// menu class toggle
 const menuToggle = document.querySelector('#menu-toggle');
 
 menuToggle.addEventListener('click', () => {
     menuToggle.classList.toggle('open-menu');
 });
+
+// quantity increment and decrement
+const minusBtn = document.querySelector('#decrease');
+const plusBtn = document.querySelector('#increase');
+const quantity = document.querySelector('#quantity');
+
+minusBtn.addEventListener('click', reduceQuantity);
+plusBtn.addEventListener('click', increaseQuantity);
+
+function reduceQuantity() {
+    let currentQty = parseInt(quantity.textContent);
+    if (currentQty <= 1) {
+        alert('Minimum is 1');
+    } else {
+        currentQty = currentQty--;
+        console.log(currentQty--);
+        quantity.textContent = currentQty;
+    }
+}
+
+function increaseQuantity() {
+    let currentQty = parseInt(quantity.textContent);
+    console.log(currentQty++);
+    currentQty = currentQty++;
+    quantity.textContent = currentQty;
+}
