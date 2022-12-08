17d1bdd0dc6bd0e2f7357276b548d82bf584af69
RalatPC <ralatrado@gmail.com>
2022-12-08 01:31:58 +0100

    added script for cart button and added cart counter

--- main.js
+++ main.js
@@ -5,14 +5,25 @@ const menuToggle = document.querySelector('#menu-toggle');
 const minusBtn = document.querySelector('#decrease');
 const plusBtn = document.querySelector('#increase');
 const quantity = document.querySelector('#quantity');
+const cartIcon = document.querySelector('#cart');
+const addCart = document.querySelector('#add-to-cart');
+const cartDiv = document.querySelector('#cart-div')
+const cartNum = document.createElement('span');
 
-
+// menu toggle
 menuToggle.addEventListener('click', () => {
     menuToggle.classList.toggle('open-menu');
 });
 
 minusBtn.addEventListener('click', reduceQuantity);
 plusBtn.addEventListener('click', increaseQuantity);
+addCart.addEventListener('click', addToCart);
+
+function addToCart() {
+    cartNum.innerHTML = quantity.textContent;
+    cartNum.classList.add('cart-counter');
+    cartDiv.appendChild(cartNum);
+}
 
 function reduceQuantity() {
     let currentQty = parseInt(quantity.textContent);
@@ -21,7 +32,7 @@ function reduceQuantity() {
     } else {
         currentQty = currentQty--;
         console.log(currentQty--);
-        quantity.textContent = currentQty;
+        quantity.innerText = currentQty;
     }
 }
 
